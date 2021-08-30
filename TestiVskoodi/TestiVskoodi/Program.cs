7954372ad1beb04fd8dcb798a4ce5b8a52fb57ee
Asko Salmenaho <Asko.Salmenaho@vuoksi.fi>
2021-08-30 14:57:00 +0300

    VsKoodi
    Hei maailma

---
+++ TestiVskoodi/TestiVskoodi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestiVskoodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
